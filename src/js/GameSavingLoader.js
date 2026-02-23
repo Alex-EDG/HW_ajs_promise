0db8d47ea270c47fb5bb1803cd3df918263da7c8
Alex-EDG <AlexEDG75@GMail.Com>
2026-02-23 20:51:42 +0300

    Update files

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,16 +1,12 @@
 import read from './reader';
 import json from './parser';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static load() {
-    return new Promise((resolve) => {
-      const data = read();
-      data.then((response) => {
-        const value = json(response);
-        return value;
-      }).then((value) => {
-        resolve(value);
-      });
-    });
+    return read().then(buffer => json(buffer)).then(data => {
+      const obj = JSON.parse(data);
+      return new GameSaving (obj.id, obj.created, obj.userInfo);
+    }).catch(error => error);
   };
 };
--- src/js/__tests__/GameSavingLoader.test.js
+++ src/js/__tests__/GameSavingLoader.test.js
@@ -1,18 +1,10 @@
 import GameSavingLoader from '../GameSavingLoader';
+import GameSaving from '../GameSaving';
 
-test('Проверка GameSavingLoader - success', (done) => {
-  const expected = JSON.stringify({
-    id: 9,
-    created: 1546300800,
-    userInfo: {
-      id: 1,
-      name: 'Hitman',
-      level: 10,
-      points: 2000
-    }
-  });
-  GameSavingLoader.load().then((saving) => {
-    expect(saving).toEqual(expected);
-    done();
+const obj = new GameSaving(9, 1546300800, { id: 1, name: 'Hitman', level: 10, points: 2000 });
+test('Проверка GameSavingLoader - success', () => {
+
+  return GameSavingLoader.load().then(saving => {
+    expect(saving).toEqual(obj);
   });
 });
